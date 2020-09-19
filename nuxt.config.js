cbf31a6277a5b140c298329e04f87bd5762b774d
MichiWoo <michiwoo.web@gmail.com>
2020-09-19 16:08:16 -0500

    🐛 FIX: Corrección 2 build agregando talwind

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,7 +47,7 @@ export default {
     // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module',
     // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
-    //@nuxtjs/tailwindcss',
+    '@nuxtjs/tailwindcss',
     '@nuxtjs/fontawesome',
   ],
   /*
